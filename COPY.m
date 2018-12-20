869d3f26bd5f11d09959ca8a29f6376e22f1ac6e
12Me21 <12Me21.mc@gmail.com>
2018-12-20 17:17:55 -0500

    update copy I guess

--- COPY.m
+++ COPY.m
@@ -5,12 +5,12 @@
 
 ** Copy data between arrays or strings
 ``` sbsyntax
-COPY dest_array [, dest_start%], source_array [[, source_start%], source_length%]
+COPY dest [, dest_start%], source [[, source_start%], source_length%]
 ```
 |* Parameter       | Description                                    |
-| `dest_array`     | array or string to copy *to*. will be extended if shorter than the copied data |
+| `dest`           | array or string to copy *to*. Strings and 1D arrays will be extended if there isn't enough room for the data |
 | `dest_start%`    | starting position in dest_array                |
-| `source_array`   | array or string to copy *from*                 |
+| `source`         | array or string to copy *from*                 |
 | `source_start%`  | position in source_array to start copying from |
 | `source_length%` | number of items to copy                        |
 ``` smilebasic
@@ -24,7 +24,7 @@ COPY B,A 'copy data from A to B
 COPY dest_array[] [, dest_start%], source_label$ [, source_length%]
 ```
 |* Parameter       | Description                     |
-| `dest_array[]`   | array to copy to. will be extended if shorter than the copied data |
+| `dest_array[]`   | array to copy to. 1D arrays will be extended if there isn't enough room for the data |
 | `dest_start%`    | starting position in dest_array |
 | `source_label$`  | `DATA` label to copy from       |
 | `source_length%` | number of items to copy         |
