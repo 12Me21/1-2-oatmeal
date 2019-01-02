58b6c56dcd1a1b6234007710be177723c43d7789
12Me21 <12Me21.mc@gmail.com>
2019-01-02 17:46:37 -0500

    a

--- COPY.m
+++ COPY.m
@@ -2,10 +2,6 @@
 #+TITLE
 + Copy data between arrays or strings
 + Read DATA into an array
-
-[[BUTTON][BUTTON()]]
-[[BUTTON]]
-
 ** Copy data between arrays or strings
 ``` sbsyntax
 COPY dest [, dest_start%], source [[, source_start%], source_length%]
@@ -39,12 +35,12 @@ DATA 10,20,30
 ```
 ** Version Information
 *** 3.2.0
-Fixed crash when COPY was called without any arguments [fn:1]
+Fixed crash when COPY was called without any arguments [[#1]]
 *** 3.5.2
-COPY now allows the source length to be 0 [fn:2] [fn:3]
+COPY now allows the source length to be 0 [[#2]] [[#3]]
 ** References
-[fn:1] SmileBoom "Fixes in Ver. 3.2.0 (June 17, 2015)" [[http://smilebasic.com/debug/archive/]]
-[fn:2] SmileBoom "Bug Fixes in Ver. 3.5.2 (June 24, 2017)" [[http://smilebasic.com/en/debug/archive/]]
-[fn:3] SquareFingers "Empty COPY source array gives error." [[https://smilebasicsource.com/forum?ftid=1263]]
+[[1]] SmileBoom "Fixes in Ver. 3.2.0 (June 17, 2015)" [[http://smilebasic.com/debug/archive/]]
+[[2]] SmileBoom "Bug Fixes in Ver. 3.5.2 (June 24, 2017)" [[http://smilebasic.com/en/debug/archive/]]
+[[3]] SquareFingers "Empty COPY source array gives error." [[https://smilebasicsource.com/forum?ftid=1263]]
 -----
 #+NAVIGATION
