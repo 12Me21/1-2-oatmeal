09dd317ab56c3560c790ee2959a8d16804b61296
12Me21 <12Me21.MC@gmail.com>
2019-01-17 15:41:57 -0500

    add diagram for arcs

--- GCIRCLE.m
+++ GCIRCLE.m
@@ -10,7 +10,8 @@ GCIRCLE center_x%, center_y%, radius% , start_angle%, end_angle% [, sector_flag%
 |`center_x%`, `center_y%`|The center of the circle|
 |`radius%`|The radius|
 |`color%`|The color. If omitted, the color set by [[GCOLOR]] will be used|
-|`start_angle%`, `end_angle%`|The start/end angles of the arc. It will be drawn counterclockwise from `start_angle%` to `end_angle%`|
+|`start_angle%`, `end_angle%`|The start/end angles of the arc. It will be drawn counterclockwise from `start_angle%` to `end_angle%`
+[[http://kland.smilebasicsource.com/i/zatel.png]]|
 |`sector_flag%`|If this is not 0, lines will be drawn from the ends of the arc to the center|
 ** Examples
 Drawing a filled circle
