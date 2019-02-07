fa99d555cdaacefb0651820450375e59c66f8a92
12Me21 <12Me21.MC@gmail.com>
2019-02-07 17:16:56 +0000

    a

---
+++ ABS.m
@@ -0,0 +1,20 @@
+#+NAVIGATION
+#+TITLE
+Returns the absolute value of a number.
+`ABS(nan)` is `nan`, and `ABS(-2147483648)` is `-2147483648`.
+** Syntax
+```sbsyntax
+ABS number OUT absolute_value
+```
+|*Input|Description|
+|`number`|An integer or double|
+|*Output|Description|
+|`absolute_value`|The absolute value of `number`|
+** Examples
+```smilebasic
+PRINT ABS(10) '10
+PRINT ABS(-10) '10
+PRINT ABS(-1.5) '10
+```
+** See also
++ [[SGN]]
