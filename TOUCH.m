e81546ef9fda4483ce7f1ff82a5dfda5964bc4b0
12Me21 <12Me21.MC@gmail.com>
2019-02-22 17:26:37 +0000

    Update TOUCH.m

--- TOUCH.m
+++ TOUCH.m
@@ -8,7 +8,11 @@ TOUCH [system%] OUT touch_time%, touch_x%, touch_y%
 |*Input|Description|
 |`system%`|Which 3DS to get the information from, when multiplayer is enabled|
 |`touch_time%`|The number of frames that the screen has been touched for, or 0 if it's not being touched|
-|`touch_x%`, `touch_y%`|The position of the touch. This will always be at least 5 pixels from the edge of the screen|
+|`touch_x%`, `touch_y%`|The position of the touch (in pixels). This will always be at least 5 away from the edge of the screen{
+|*System|Touch Screen Resolution|Range|
+|3DS|320x240|5,5 to 314,234|
+|Wii U|Unknown (Depends on XSCREEN)|Unknown (Depends on XSCREEN)|
+}|
 ** Examples
 Control the position of a sprite using the touch screen
 ``` smilebasic
