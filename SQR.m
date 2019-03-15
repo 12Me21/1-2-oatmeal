a95d37fa667852c0877bd93fc6df16d45d9519da
12Me21 <12Me21.MC@gmail.com>
2019-03-15 13:27:22 -0400

    Create SQR.m

---
+++ SQR.m
@@ -0,0 +1,16 @@
+#+NAVIGATION
+#+TITLE
+Calculates the square root of a number.
+** Syntax
+```sbsyntax
+SQR number# OUT square_root#
+```
+** Examples
+```smilebasic
+PRINT SQR(16) '4
+PRINT SQR(2) '1.41421356
+PRINT SQR(7)*SQR(7) '7
+```
+** Errors
+|*Error|Cause|
+|`Out of range`|`number#` is less than 0|
