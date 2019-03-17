9f7b1258a5fdb9a00e5bf379d1c25cd8f4c51614
12Me21 <12Me21.MC@gmail.com>
2019-03-17 13:31:24 -0400

    Create MOD.m

---
+++ MOD.m
@@ -0,0 +1,15 @@
+#+NAVIGATION
+#+TITLE
+Gets the remainder of an integer division.
+** Syntax
+``` sbsyntax
+a% MOD b%
+```
+** Examples
+``` smilebasic
+' 11/4 = 2 remainder 3
+PRINT 11 DIV 4 '2 (integer quotient)
+PRINT 11 MOD 4 '3 (remainder)
+```
+** See Also
++ [[DIV]]
