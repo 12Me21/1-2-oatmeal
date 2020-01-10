c62bb4507cf30a4585f2c380e95fe4b0b7b2cac7
12Me21 <12Me21.MC@gmail.com>
2020-01-10 12:16:49 -0500

    Create MICDATA.m

---
+++ MICDATA.m
@@ -0,0 +1,26 @@
+#+NAVIGATION
+#+TITLE
+Get 1 sample from the mic buffer.
+Mic must be enabled with [[XON MIC]] first, but [[MICDATA]] can still be used if the mic is enabled but not currently recording.
+** Syntax
+``` sbsyntax
+MICSTART [position%] OUT sample%
+```
+|*Input|Description|
+|`position%`|Position of sample to get. Defaults to [[MICPOS]].
+Can't be negative, but treated as `position% MOD MICSIZE` if larger than `MICSIZE`.|
+|*Output|Description|
+|`sample%`|See [[MICSTART]] for sample formats.|
+** Examples
+``` smilebasic
+XON MIC
+MICSTART 3,3,0
+WHILE 1
+ ?MICDATA()
+WEND
+```
+** See Also
++ [[MICSTART]]
++ [[MICSAVE]]
+-----
+#+NAVIGATION
