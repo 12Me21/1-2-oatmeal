26c427579dc64d626250a75aeed72a3a17c8f0fe
12Me21 <12Me21.MC@gmail.com>
2020-01-10 13:14:59 -0500

    Create MICSTOP.m

---
+++ MICSTOP.m
@@ -0,0 +1,16 @@
+#+NAVIGATION
+#+TITLE
+Stop recording audio.
+The mic must be enabled with [[XON MIC]] first.
+** Syntax
+``` sbsyntax
+MICSTOP
+```
+** Possible Errors
+|*Error|Cause|
+|`Mic is not available`|Mic is not enabled (Use [[XON MIC]] first)|
+|`Illegal function call`|Wrong number of arguments|
+** See Also
++ [[MICSTART]]
+-----
+#+NAVIGATION
